8f4c2c9a2a4ae4f2ec930da80eaf6dc7bef925e4
Wen-Chien Chen <wenchienchen@asana.com>
2024-10-02 18:04:46 -0400

    Don't provide completions in GraphQL tags

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@ import * as vscode from "vscode";
 import { CompletionItemsCache } from "./completion_items_cache";
 import { CompletionItemsCacheImpl } from "./completion_items_cache_impl";
 
+const openGraphQLTag = /gql`[^`]*$/;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -42,7 +44,9 @@ export function activate(context: vscode.ExtensionContext) {
         {
             provideCompletionItems(doc: vscode.TextDocument, position: vscode.Position) {
                 const wordRange = doc.getWordRangeAtPosition(position);
-                if (wordRange === undefined) {
+                // Don't provide completions if the cursor is inside a gql`` template literal to
+                // avoid conflicting with fragment name completions from the GraphQL extension.
+                if (wordRange === undefined || isInGraphQLTag(doc, position)) {
                     return new vscode.CompletionList([], true);
                 }
 
@@ -59,3 +63,28 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function deactivate() {}
+
+/**
+ * Returns an approximation for whether the cursor is inside a gql`` template literal by searching
+ * backwards in the document. Returns true if "gql`" is encountered before a standalone "`" or
+ * a semicolon.
+ */
+function isInGraphQLTag(doc: vscode.TextDocument, position: vscode.Position): boolean {
+    const textBeforeCursor = doc.lineAt(position.line).text.slice(0, position.character);
+    if (openGraphQLTag.test(textBeforeCursor)) {
+        return true;
+    }
+    if (textBeforeCursor.includes("`") || textBeforeCursor.includes(";")) {
+        return false;
+    }
+    for (let i = position.line - 1; i >= 0; i--) {
+        const line = doc.lineAt(i).text;
+        if (openGraphQLTag.test(line)) {
+            return true;
+        }
+        if (line.includes("`") || line.includes(";")) {
+            return false;
+        }
+    }
+    return false;
+}
