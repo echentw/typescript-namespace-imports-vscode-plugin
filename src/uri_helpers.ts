87945511e0003a7171b422ba6682541c2fabee25
Eric Chen <ericchen098@gmail.com>
2025-07-20 01:12:37 -0700

    CapitalCase -> camelCase

--- src/uri_helpers.ts
+++ src/uri_helpers.ts
@@ -26,7 +26,7 @@ function _lookForPathWithBaseUrl(uriPath: string, baseUrlMap: Record<string, str
 
 export function uriToModuleName(uri: vscode.Uri): string {
     const fileName = Path.basename(uri.path, uri.path.endsWith("ts") ? ".ts" : ".tsx");
-    return _.upperFirst(_.camelCase(fileName));
+    return _.camelCase(fileName);
 }
 
 export function uriToCompletionItem(
