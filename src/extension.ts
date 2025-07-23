77f552d769cf1e0ac6872fd4f83cab0abe9f78e4
Eric Chen <ericchen098@gmail.com>
2025-07-22 21:40:53 -0700

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,6 @@ const openGraphQLTag = /gql`[^`]*$/;
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     const workspaceFolders = vscode.workspace.workspaceFolders;
-
     if (workspaceFolders === undefined) {
         console.warn("No workspace folder. typescript-namespace-imports-vscode-plugin will not work");
         return;
